4bc936e8ff6b6f2767530a3ac68b1844fd0d3757
Evan Phillips <evanap003300@gmail.com>
2025-07-05 20:18:22 -0400

    Adding files

--- word_parsing/build_json.py
+++ word_parsing/build_json.py
@@ -1,24 +1,13 @@
 from .extract_workout_info import extract_workout_info
 import json
-import os 
-# Output format:
-# workout_log = [
-#     {
-#         "exercise": "exercise_name",
-#         "sets": [
-#             {"reps": 10, "weight": 100},
-#             ...
-#         ]
-#     },
-#     ...
-# ]
+import os
 
 def build_json():
+    # Extract lists from NLP extraction
     exercise_list, sets_list, reps_list, weight_list = extract_workout_info()
     
     workout_log = []
-    
-    rep_weight_idx = 0  # Index into flat reps/weights list
+    rep_weight_idx = 0  # Tracks index into reps/weights arrays
 
     for i, exercise in enumerate(exercise_list):
         exercise_data = {
@@ -26,17 +15,20 @@ def build_json():
             "sets": []
         }
 
-        if i < len(sets_list):
-            num_sets = sets_list[i]
+        # Get number of sets for this exercise, default to 1 if missing
+        num_sets = sets_list[i] if i < len(sets_list) else 1
 
-            for _ in range(num_sets):
-                if rep_weight_idx < len(reps_list) and rep_weight_idx < len(weight_list):
-                    set_data = {
-                        "reps": reps_list[rep_weight_idx],
-                        "weight": weight_list[rep_weight_idx]
-                    }
-                    exercise_data["sets"].append(set_data)
-                    rep_weight_idx += 1
+        for _ in range(num_sets):
+            # Ensure we don't go out of bounds
+            if rep_weight_idx < len(reps_list) and rep_weight_idx < len(weight_list):
+                set_data = {
+                    "reps": reps_list[rep_weight_idx],
+                    "weight": weight_list[rep_weight_idx]
+                }
+                exercise_data["sets"].append(set_data)
+                rep_weight_idx += 1
+            else:
+                break  # No more rep/weight pairs to use
 
         workout_log.append(exercise_data)
 
@@ -47,5 +39,5 @@ def build_json():
     
     with open(json_path, 'w') as f:
         json.dump(workout_log, f, indent=2)
-    
+
     return workout_log
